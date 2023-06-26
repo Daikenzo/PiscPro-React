a1f4123d9f3b236540c4f14f126694521f540ef3
Charles BRU(Daikenzo) <charles.bru@lapiscine.pro>
2023-06-26 14:36:48 +0200

    finish exo exo1

--- Project/assets/js/index-01.js
+++ Project/assets/js/index-01.js
@@ -1,54 +1,72 @@
 // Project 01 - Index.js
 // var & const global
-let result, consigneMess; 
-
-// refference
+let _result, _consigneMess; 
+// reference
 const button = document.querySelector('button'); // cibler le button 
-// // au click button
-// button.addEventListener('click', () => {
-//     console.log("Click on")
 
-// // cibler text
-// const text = document.querySelector('#p-loremExample');
+// Init
+init();
+// Flexible function
+    // Create Node Element
+const createNodeElement = (tagType, classNames, contentTxt = "", imgsrc = "") =>{
+    const nodeElement = document.createElement(tagType);
+    nodeElement.setAttribute("class", classNames);
+    nodeElement.setAttribute('src', imgsrc);
+    nodeElement.textContent = contentTxt;
+    return nodeElement;
+};
+// Run
 
-
-// //mask text
-// text.style.display = "none";
-
-// // function
-
-// });
-//init
-
-
-button.addEventListener('click', () =>{
-    // créer article
-    const articleEl = document.createElement('article');
-
-    const divRoot = document.querySelector("#root");
-
-    divRoot.appendChild(articleEl);
-
-    // create title
-    const titleEl = document.createElement("h2");
-
-    titleEl.textContent = "Hello here";
-    articleEl.appendChild(titleEl);
-
-    //Create Image
-    const imgElement = document.createElement("img");
-    imgElement.setAttribute('src', "https://developer.mozilla.org/pimg/aHR0cHM6Ly9zLnprY2RuLm5ldC9BZHZlcnRpc2Vycy84NTg0Mzc2NzgxOTM0ZWZjYWYxZTU1MGYyMzQ2OGY3NS5wbmc%3D.M4peha%2FjPCrnRjeB5fGs15ztBrN0zSHetSrRSIuMYnk%3D");
-
-    articleEl.appendChild(imgElement)
-
-    // Create text
-    const textEl = document.createElement("p");
-    textEl.textContent ="bienvenue à la piscine !";
-    articleEl.appendChild(textEl)
-})
-
-function init(){
+exo1();
 
 
+
+
+// Exo
+function exo1(){
+    button.addEventListener('click', () =>{
+        // Create Article Element (from Root div)
+        const divRoot = document.querySelector('#root')
+        // créer article
+        const articleEl = createNodeElement("article")
+        divRoot.appendChild(articleEl);
+
+        // create title    
+        const titleEl = createNodeElement("h2",
+        "title-piscine", 
+        "Title  Article");
+        articleEl.appendChild(titleEl);
+    
+        //Create Image
+        const imgElement = createNodeElement(
+            "img",
+            "img-piscine", 
+            "", 
+            "https://developer.mozilla.org/pimg/aHR0cHM6Ly9zLnprY2RuLm5ldC9BZHZlcnRpc2Vycy84NTg0Mzc2NzgxOTM0ZWZjYWYxZTU1MGYyMzQ2OGY3NS5wbmc%3D.M4peha%2FjPCrnRjeB5fGs15ztBrN0zSHetSrRSIuMYnk%3D"
+            );
+        articleEl.appendChild(imgElement)
+    
+        // Create text
+        const textEl = createNodeElement("p","text-piscine", "Bienvenue ici");
+        articleEl.appendChild(textEl)
+    });
 };
 
+
+
+//Init
+function init(){
+    //Reference initilisation
+// exo 1
+};
+
+// demo/Test
+function _demoTest(){
+    button.addEventListener('click', () => {
+        console.log("Click on") // Demo Test
+    });
+    // cibler text
+    //const text = document.querySelector('#p-loremExample');
+    //mask text
+    //text.style.display = "none";
+}
