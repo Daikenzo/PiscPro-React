20a13aac3db1832bb087a66923c50a1e583dc338
Charles BRU(Daikenzo) <charles.bru@lapiscine.pro>
2023-07-12 12:07:50 +0200

    maj css card

--- Project/08_react_scrud/src/components/ShowCoworkings.jsx
+++ Project/08_react_scrud/src/components/ShowCoworkings.jsx
@@ -4,12 +4,12 @@ const ShowCoworking = ({coworking}) =>{
       }
 
     return (
-        <article key={coworking.id} className="App-form">
+        <article key={coworking.id} className="App-card">
             <h3>{coworking.name}</h3>
             <p>{coworking.address}</p>
             <p>{coworking.phone}</p>
             <img src={coworking.img} alt={coworking.name} />
-            <button onClick={handleDelete} className="btn btn-delete-coworking">Suprrimer le coworking</button>
+            <button className="App-btn" onClick={handleDelete} className="btn btn-delete-coworking">Suprrimer le coworking</button>
       </article>
     );
 };
