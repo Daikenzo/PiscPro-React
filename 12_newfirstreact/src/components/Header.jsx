7157e91869b5815db59c8e755c010da1b8a92d95
Daikenzo Wolfdry Sekaikasou <charles.bru@lapiscine.pro>
2023-11-07 17:10:43 +0100

    fix navbar css

--- 12_newfirstreact/src/components/Header.jsx
+++ 12_newfirstreact/src/components/Header.jsx
@@ -6,8 +6,9 @@ const Header = ({logo}) =>{
         <header className="App-header">
             <nav className="header-nav">
               <ul className="App-nav">
-                <li className="nav-item"><a className="App-link" href="#"></a>TitleSite</li>
-                <li className="nav-item"><a className="App-link" href="#articles"></a>articles</li>
+                <li className="nav-item"><a id="titlesite" className="App-link App-title" href="#"><h1>TitleSite</h1></a></li>
+                <li className="nav-item"><a className="App-link" href="#articles">articles</a></li>
+                <li className="nav-item"><a className="App-link" href="#articles">articles</a></li>
               </ul>
             </nav>
         </header>
--- 12_newfirstreact/src/components/Main.jsx
+++ 12_newfirstreact/src/components/Main.jsx
@@ -1,6 +1,7 @@
 import ArticlesDisplay from "./articles/articles";
 
 const Main = () =>{
+
     return (
         <main className="App-main">
             <ArticlesDisplay />
